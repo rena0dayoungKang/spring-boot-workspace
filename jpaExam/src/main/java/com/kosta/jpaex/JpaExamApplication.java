0a80e18775c8afd754912df90836e74f71c08f65
rena <dayoungrenakang@gmail.com>
2024-10-22 09:24:29 +0900

    Update JpaExamApplication.java

--- jpaExam/src/main/java/com/kosta/jpaex/JpaExamApplication.java
+++ jpaExam/src/main/java/com/kosta/jpaex/JpaExamApplication.java
@@ -1,10 +1,7 @@
 package com.kosta.jpaex;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.kosta.jpaex.service.ActorService;
 
 @SpringBootApplication
 public class JpaExamApplication {
